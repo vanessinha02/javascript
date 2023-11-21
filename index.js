6a06c68d9c9b2ac213b4cac5147b990447f19b38
vanessinha02 <134697373+vanessinha02@users.noreply.github.com>
2023-11-21 08:32:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+<script>
 let botaoCor = document.getElementById("botaoMudaCor");
 botaoCor.addEventListener("click", function() {
     document.body.style.backgroundColor = "lightblue";
 });
-
+</script>
+    </body>
+    </html>
